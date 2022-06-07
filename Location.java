241b37f241f9a7a92159b1a4f921ea632ac36136
Namakudami <dsosiala0@gmail.com>
2022-06-07 19:52:33 -0400

    Create Location.java

---
+++ Location.java
@@ -0,0 +1,30 @@
+import core.data.*;
+public class Location{
+    private float temp;
+    private String name;
+    private DataSource ds;
+    private final String TEMP = "temp_c";
+
+    public Location(String name, String link){
+        this.name = name;
+        ds = DataSource.connect(link);
+        ds.load();
+        temp = ds.fetchFloat(TEMP);
+    }
+
+    public String name(){
+        return name;
+    }
+    
+    public float temp(){
+        return temp;
+    }
+
+    public String toString(){
+        return "Temperature in " + name + " is " + temp + " celcius";
+    }
+
+    public void printUsageString(){
+        ds.printUsageString();
+    }
+}
