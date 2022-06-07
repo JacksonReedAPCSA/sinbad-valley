6b9bce69beef87246253edad1d31da3781a0c3bd
Namakudami <dsosiala0@gmail.com>
2022-06-07 15:06:37 -0400

    stuff

--- SinbadValley.java
+++ SinbadValley.java
@@ -1,4 +1,7 @@
 public class SinbadValley{
     public static void main(String[] args) {
+        Location NY = new Location("New York", "https://w1.weather.gov/xml/current_obs/KNYC.xml");
+        System.out.println(NY.name() + "\'s temperature: " + NY.temp() + " celcius.");
+        NY.printUsageString();
     }
 }
