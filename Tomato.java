4a393e0860a53ebab44434d4f63d510afb98a562
Namakudami <dsosiala0@gmail.com>
2022-06-10 14:12:19 -0400

    Update Tomato.java

--- Tomato.java
+++ Tomato.java
@@ -1,7 +1,7 @@
 public class Tomato extends Crop {
   private static final double lTemp = 12.7;
   private static final double uTemp = 26.6;
-  private static final double sellVal = 3.0;
+  private static final double sellVal = 3.2;
   private static final double buyVal = 2.0;
   private static final String name = "Tomato";
   private static final int fullGrowth = 6;
